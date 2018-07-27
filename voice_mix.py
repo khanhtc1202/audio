e4fa868bf29dc537c41a36373ca92a49c89a20cc
khanhub <khanh.tran@uzabase.com>
2018-07-28 03:28:44 +0900

    mix finish 1

--- voice_mix.py
+++ voice_mix.py
@@ -1,6 +1,5 @@
 from pydub import AudioSegment
 
-
 base_path = "./public/voices"
 
 
@@ -9,21 +8,26 @@ def read_sound_from_source(path):
 
 
 def cut_sound(sound, head, tail):
-    return sound[head*1000 : tail: 1000]
+    return sound[head*1000 : tail*1000]
 
 
-def combine_voice(s1, s2, export=True, path=base_path+"/mix/combined.wav"):
+def combine_voice(s1, s2, export=True, path=base_path+"/mix/combined.mp3"):
     combined = s1.overlay(s2)
     if export:
-        combined.export(path, format='wav')
+        combined.export(path, format='mp3')
     return combined
 
 
+def make_noise(sound, save_to=base_path+"/mix/combined.mp3"):
+    noise = read_sound_from_source(base_path + "/noise/radio-noise.wav")
+    for i in xrange(1, len(sound)/len(noise)):
+        noise.append(noise)
+    combine_voice(sound, noise, path=save_to)
+
+
 def main():
-    sound1 = read_sound_from_source(base_path + "/for_visual/chorus_1.wav")
-    sound2 = read_sound_from_source(base_path + "/noise/radio-noise.wav")
-    noise = cut_sound(sound2, 36, 51)
-    combine_voice(sound1, noise)
+    sound1 = read_sound_from_source(base_path + "/mix/whams.mp3")
+    make_noise(sound=sound1)
 
 
 if __name__ == '__main__':
