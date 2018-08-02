c482497e352154811934e5abaf5d20d2bf32fd0c
khanhub <khanh.tran@uzabase.com>
2018-08-02 22:24:45 +0900

    new env

--- voice_mix.py
+++ voice_mix.py
@@ -1,7 +1,8 @@
 from pydub import AudioSegment
 
 base_path = "./public/voices"
-
+default_export_path = base_path+"/mix/combined.mp3"
+default_noise_path = base_path + "/noise/radio-noise.wav"
 
 def read_sound_from_source(path):
     return AudioSegment.from_file(path)
@@ -11,15 +12,15 @@ def cut_sound(sound, head, tail):
     return sound[head*1000 : tail*1000]
 
 
-def combine_voice(s1, s2, export=True, path=base_path+"/mix/combined.mp3"):
+def combine_voice(s1, s2, export=True, path=default_export_path):
     combined = s1.overlay(s2)
     if export:
         combined.export(path, format='mp3')
     return combined
 
 
-def make_noise(sound, save_to=base_path+"/mix/combined.mp3"):
-    noise = read_sound_from_source(base_path + "/noise/radio-noise.wav")
+def make_noise(sound, save_to=default_export_path):
+    noise = read_sound_from_source(default_noise_path)
     for i in xrange(1, len(sound)/len(noise)):
         noise.append(noise)
     combine_voice(sound, noise, path=save_to)
